83f6d3cda58b7f8cccfc34ffc458ef48f21f2d66
iwt-katharinabaeuml <katharina.baeuml@immowelt.de>
2023-03-21 14:03:40 +0100

    e2e test added

--- apps/app-e2e/src/e2e/app.cy.ts
+++ apps/app-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,67 @@
-import { getGreeting } from '../support/app.po';
-
 describe('app', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    cy.visit('localhost:4200');
+    cy.get('#description').type('Osterbrot backen');
+    cy.get('#datepicker').type('2023-03-21', { force: true });
+    cy.get('#handleOpen').click();
+    cy.get('#MenuOne').click();
+    cy.get('#add_button').click();
+    cy.get('#description').clear();
+  });
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should add new todo', () => {
+    cy.get('#description').type('Nikolausmuetze aufsetzen');
+    cy.get('#datepicker').type('2023-12-06', { force: true });
+    cy.get('#description').click();
+    cy.get('#handleOpen').click();
+    cy.get('#MenuThree').click();
+    cy.get('#add_button').click();
+    cy.get('#description').clear();
+    cy.get('#todo-list')
+      .get('li#item-0 .item-description:first')
+      .should('contain', 'Osterbrot backen');
+    cy.get('#todo-list')
+      .get('li#item-0 .item-priority:first')
+      .should('contain', '!!!');
+    cy.get('#todo-list')
+      .get('li#item-0 .item-date:first')
+      .should('contain', '2023-03-21');
+    cy.get('#todo-list')
+      .get('li#item-1 .item-description:first')
+      .should('contain', 'Nikolausmuetze aufsetzen');
+    cy.get('#todo-list')
+      .get('li#item-1 .item-priority:first')
+      .should('contain', '!');
+    cy.get('#todo-list')
+      .get('li#item-1 .item-date:first')
+      .should('contain', '2023-12-06');
+    cy.get('#todo-list').find('li').should('have.length', 2);
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome app');
+  it('should complete a todo', () => {
+    cy.get('#todo-list').find('li').should('have.length', 1);
+    cy.get('#todo-list')
+      .get('li:first .item-description:first')
+      .should('contain', 'Osterbrot backen');
+    cy.get('#todo-list')
+      .get('li:first .item-priority:first')
+      .should('contain', '!!!');
+    cy.get('#todo-list')
+      .get('li:first .item-date:first')
+      .should('contain', '2023-03-21');
+    cy.get('#donelist').find('li').should('have.length', 0);
+    cy.get('#todo-list').find('li:first').find('[type="checkbox"]').click();
+    cy.get('#donelist').find('li:first').should('contain', 'Osterbrot backen');
+    cy.get('#donelist').find('li').should('have.length', 1);
+  });
+
+  it('should reopen a todo', () => {
+    cy.get('#todo-list').find('li:first').find('[type="checkbox"]').click();
+    cy.get('#donelist').find('li:first').should('contain', 'Osterbrot backen');
+    cy.get('#todo-list').find('li').should('have.length', 0);
+    cy.get('#donelist').find('li').should('have.length', 1);
+    cy.get('#donelist').find('li:first').find('[type="checkbox"]').click();
+    cy.get('#todo-list').find('li').should('have.length', 1);
+    cy.get('#donelist').find('li').should('have.length', 0);
   });
 });
